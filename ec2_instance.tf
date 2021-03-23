ef66e03d203b3fe82e4a8cf7765d096d7976dc1b
nazy67 <68819199+nazy67@users.noreply.github.com>
2021-03-22 21:10:30 -0500

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -10,31 +10,28 @@ resource "aws_instance" "project_ec2" {
   }
 }
 resource "aws_security_group" "project_sg" {
-  name        = "allow_22_80"
+  name        = "allow_http"
   description = "Allow TLS inbound traffic"
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name  = "allow_http"
   }
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+}
+resource "aws_security_group_rule" "http_ingress" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.project_sg.id
+}
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.project_sg.id
+}
 
   tags = {
     Name = "project_sg"
