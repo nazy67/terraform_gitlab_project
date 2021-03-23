f0f2b7c35dac3257a1de7422fecd97a504d4792e
nazy67 <68819199+nazy67@users.noreply.github.com>
2021-03-22 21:06:47 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "ec2_public_ip" {
-  value = aws_instance.second_ec2.public_ip
+  value = aws_instance.project_ec2.public_ip
 }
 output "sg_name" {
-  value = aws_security_group.second_sg.name
+  value = aws_security_group.project_sg.name
 } 
