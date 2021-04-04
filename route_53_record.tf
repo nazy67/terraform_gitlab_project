e5fff367b3e97a056753312e51591285fdf2aab6
nazy67 <68819199+nazy67@users.noreply.github.com>
2021-04-03 22:33:52 -0500

    Update route_53_record.tf

--- route_53_record.tf
+++ route_53_record.tf
@@ -5,7 +5,7 @@ data "aws_route53_zone" "dns_name" {
 
 resource "aws_route53_record" "record" {
   zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.${data.aws_route53_zone.dns_name}"
+  name    = "www.${data.aws_route53_zone.dns_name.name}"
   type    = "A"
   ttl     = "300"
   records = ["${aws_instance.project_ec2.public_ip}"]
