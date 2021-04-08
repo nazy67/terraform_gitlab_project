c2f26a5da75fbd1ead10aa439f273cdfa2aef860
nazy67 <68819199+nazy67@users.noreply.github.com>
2021-04-07 20:16:25 -0500

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -35,6 +35,14 @@ resource "aws_security_group_rule" "ssh_ingress" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.project_sg.id
 }
+resource "aws_security_group_rule" "https_ingress" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.project_sg.id
+}
 resource "aws_security_group_rule" "egress" {
   type              = "egress"
   from_port         = 0
