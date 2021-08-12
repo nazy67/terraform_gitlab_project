5715b49ce0b5ce515f630f8daecd97c59b4ca1b1
nazy67 <68819199+nazy67@users.noreply.github.com>
2021-08-11 22:12:04 -0500

    Delete user_data.sh

--- user_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-yum install -y curl policycoreutils-python perl
-curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash 
-yum install -y gitlab-ce
